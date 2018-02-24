13b3f52b579df453a08bf67ba95ebf1cc6f711b5
Maho Misumi <maho.smile606@gmail.com>
2018-02-24 14:26:27 +0900

    エラー

--- Cocoapods2/ViewController.swift
+++ Cocoapods2/ViewController.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import PKHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        HUD.flash(.sucess, deplay: 2.0)
     }
 
 
